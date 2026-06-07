c8ff0d77b06aa5644faefd5282f1397b6aa09070
houdaifa <houdaifazaidi04@gmail.com>
2026-06-07 15:29:47 +0100

    fix: global custom styled pagination

---
+++ resources/views/partials/pagination.blade.php
@@ -0,0 +1,36 @@
+@if ($paginator->hasPages())
+    <nav>
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <span class="disabled"><span>&laquo;</span></span>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <span class="disabled"><span>{{ $element }}</span></span>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <span class="active"><span>{{ $page }}</span></span>
+                    @else
+                        <a href="{{ $url }}">{{ $page }}</a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
+        @else
+            <span class="disabled"><span>&raquo;</span></span>
+        @endif
+    </nav>
+@endif
